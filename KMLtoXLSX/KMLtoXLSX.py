1d9680acbc02574200531b77fa47e0c48be5667e
THA7N7 <61599938+THA7N7@users.noreply.github.com>
2021-02-27 11:45:13 +0300

    Add file temple .KLM to .XLSX

--- KMLtoXLSX/KMLtoXLSX.py
+++ KMLtoXLSX/KMLtoXLSX.py
@@ -1,49 +1,48 @@
 ﻿#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import sys
 import openpyxl
 from os.path import join, abspath
 from bs4 import BeautifulSoup
 
 
-def description_text():
-    description = [
-        ' .KML to .XLSX ver. 1.0.1',
-        ' Программа сохраняет имена точек (тег:name) из файла «Points.kml».',
-        ' В файл «Points.xlsx» (нужен пустой файл таким именем)',
-        ' tha7n7@gmail.com freeware ©2021',
-    ]
-    return description
+def print_description_text():
+    '''
+        =================================================================
+        Parsing <name> from .KML to .XLSX ver. 1.0.2
+
+        Программа сохраняет имена точек (тег:name) из файла «Points.kml».
+        В файл «Points.xlsx» (нужен пустой файл таким именем)
+
+        tha7n7@gmail.com freeware ©2021
+        =================================================================
+    '''
+    pass
 
 
-def open_points(name):
-    xls_file = join('.', name)
-    xls_file = abspath(xls_file)
+def print_version_text():
+    '''
+        ============================================
+        Parsing <name> from .KML to .XLSX ver. 1.0.2
+        tha7n7@gmail.com freeware ©2021
+        ============================================
+    '''
+    pass
+
+
+def open_xlsx(name):
+    xlsx_file = join('.', name)
+    xlsx_file = abspath(xlsx_file)
     try:
-        return openpyxl.load_workbook(filename=xls_file, read_only=False, data_only=True)
-    except:
-        print("Points.xlsx не найден")
+        return openpyxl.load_workbook(filename=xlsx_file, read_only=False, data_only=True)
+    except FileNotFoundError:
+        print(print_description_text.__doc__)
+        print("{} не найден".format(name))
         print()
         input("Press any key to continue...")
         quit()
 
 
-def close_points(msg):
-    print(msg)
-    input("Press any key to continue...")
-    wb_points.close()
-
-
-if __name__ == '__main__':
-    description = description_text()
-    print(description[0])
-    print()
-    print(description[1])
-    print(description[2])
-    print()
-    print(description[3])
-    print()
-
+def main():
     points_list = []
     kml_file = join('.', 'Points.kml')
     with open(kml_file, 'r', encoding="utf-8") as f:
@@ -52,15 +51,22 @@ if __name__ == '__main__':
             points_list.append(nm.string)
 
     if len(points_list) > 0:
-        wb_points = open_points('Points.xlsx')
-        i = 0
+        wb_points = open_xlsx('Points.xlsx')
         sh = wb_points.active
         sh.delete_cols(1)
-        for i in range(len(points_list)):
+        for i in range(0, len(points_list)):
             sh.cell(row=1 + i, column=1).value = points_list[i]
-        print("Сохранено (" + str(len(points_list)) +")")
+        print("Сохранено (" + str(len(points_list)) + ")")
+        wb_points.save('Points.xlsx')
+        wb_points.close()
     else:
         print("Имена точек не найдены")
+        print()
+    print(print_version_text.__doc__)
 
-    wb_points.save('Points.xlsx')
-    close_points('')
+
+if __name__ == '__main__':
+    print()
+    main()
+    print()
+    input("Press any key to continue...")
